d5cafbe3b93fb3360bc0ef22a3d94d71a8040e63
Kirill <shabkir02@gmail.com>
2022-01-14 12:51:28 +0600

    remarks corrected

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '\\dist'));
-console.log(__dirname + '\\dist')
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
